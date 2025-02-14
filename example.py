c000ec92983e4373991679912afd1a39cfdb5c8d
Cypress <cypress@lumafield.com>
2025-02-14 12:18:03 -0500

    Use json serialization

--- example.py
+++ example.py
@@ -1,20 +1,22 @@
+from google.protobuf.json_format import MessageToJson, Parse
+
 from datatypes_pb2 import SimpleMessage, SimpleMessageClone
 
 
 def main():
-    message: SimpleMessage = SimpleMessage(content="hi there", id="asdf")
+    message: SimpleMessage = SimpleMessage(content="hi there")
     print("Original message:", message)
 
-    serialized = message.SerializeToString()
+    serialized = MessageToJson(message)
     print("Serialized:", serialized)
 
     message_2 = SimpleMessageClone()
-    message_2.ParseFromString(serialized)
+    Parse(serialized, message_2)
     print("Deserialized message:", message_2)
 
-    serialized_2 = message_2.SerializeToString()
+    serialized_2 = MessageToJson(message_2)
     message_3 = SimpleMessage()
-    message_3.ParseFromString(serialized_2)
+    Parse(serialized_2, message_3)
     print("Deserialized message 2:", message_3)
 
 
